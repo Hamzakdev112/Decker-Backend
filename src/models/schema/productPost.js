6d3d8c89a707f439fe6e4afc9de32d7cc067d44e
hkkhan112 <102105224+hkkhan112@users.noreply.github.com>
2023-02-16 23:29:30 +0500

    Add files via upload

---
+++ src/models/schema/productPost.js
@@ -0,0 +1,32 @@
+const mongoose=require('mongoose');
+
+
+const productSchema =new mongoose.Schema({
+
+    title: {
+        type:String,
+        required: [true, "Please enter product title"]
+    },
+    description: {
+        type:String,
+        required: [true, "Please enter product description"]
+    },
+    userId: {
+        type:mongoose.Types.ObjectId,
+        ref: "User",
+        required: [true, "Please enter product userId"]
+    },
+    price: {
+        type:Number,
+        required: [true, "Please enter product price"]
+    },
+    views: {
+        type:Number,
+        default: 0
+    },
+},{
+    timestamps:true
+} )
+
+
+module.exports = mongoose.model('Product_Post',productSchema);
