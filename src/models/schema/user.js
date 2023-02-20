5f5343bcec7c48676f9134d435eae4cc619714ea
MuhammadHARIS46 <muhammadhariskuk123@gmail.com>
2023-02-20 16:42:36 +0500

    userType added in user

--- src/models/schema/user.js
+++ src/models/schema/user.js
@@ -1,42 +1,68 @@
-
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const createUserSchema = new mongoose.Schema({
-
-    firstName: {
-        type: String,
-        required: true
-    },
-
-    lastName: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-
-    gender: {
-        type: String,
-        requierd: true
-    },
-    password: {
-        type: String,
-        required: true
-
-    },
-    phone: {
-        type: String,
-        required: true
-    },
-image:{
+  firstName: {
     type: String,
-}
+    required: true,
+  },
 
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
 
-})
+  gender: {
+    type: String,
+    requierd: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+  },
+  userType :{
+    type:String
+  },
+  education:{
+    type:String
+  },
+  domain:{
+    type:String
+  },
+  reason:{
+    type:String
+  },
+  
+  jobStatus:{
+    type:Boolean
+  },
+  futureInterest:{
+    type:String
+  },
+  enterpriseName:{
+    type:String
+  },
+  enterpriseSize:{
+    type:String
+  },
+  startupName:{
+    type:String
+  },
+  startupSize:{
+    type:String
+  }
+});
 
-const createUser = new mongoose.model('User', createUserSchema)
+const createUser = new mongoose.model("User", createUserSchema);
 
 module.exports = createUser;
