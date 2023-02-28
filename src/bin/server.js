11e1065124c61e05431484b2619290577815df3a
Hamzakdev112 <125876495+Hamzakdev112@users.noreply.github.com>
2023-02-28 16:16:11 +0500

    Update server.js

--- src/bin/server.js
+++ src/bin/server.js
@@ -8,13 +8,11 @@ const userRoute = require('../Routes/user')
 const chatRoute = require('../Routes/chat')
 const paymentRoute = require('../Routes/payment')
 var bodyParser = require('body-parser');
-const fileUpload = require('express-fileupload')
 const CookieParser = require('cookie-parser');
-// const io = require('socket.io')(3000)
-
-// io.on('connection', socket =>{
-//   console.log(socket.id)
-// })
+const commentRoute = require('../Routes/comments')
+const likeRoute = require('../Routes/likes')
+var bodyParser = require('body-parser');
+const session=require('express-session')
 
 
 //MIDDLEWARES
@@ -22,27 +20,8 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 app.use(CookieParser())
-
-
 app.use(bodyParser.json());
-=======
-const commentRoute = require('../Routes/comments')
-const likeRoute = require('../Routes/likes')
-var bodyParser = require('body-parser');
-const cookieParser = require("cookie-parser");
-const session=require('express-session')
-require("../bootstrap/index");
-const postRoute = require("../Routes/post");
-const userRoute = require("../Routes/user");
-const paymentRoute = require("../Routes/payment");
-const fileUpload = require("express-fileupload");
-const CookieParser = require("cookie-parser");
-const chatRoute = require("../Routes/chat");
-app.use(express.json());
-app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.urlencoded({ extended: true }));
-
 app.use(
   session({
     secret: "my-secret-key",
@@ -50,25 +29,6 @@ app.use(
     saveUninitialized: true,
   })
 );
-
-require("../bootstrap/index");
-
-//Routes
-
-app.use('/api/posts', postRoute)
-app.use('/api/users', userRoute)
-app.use('/api/payment', paymentRoute)
-app.use('/api/chat', chatRoute)
-
-=======
-app.use(cors());
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(CookieParser());
-
-app.use(bodyParser.json());
-
 
 //Routes
 app.use('/api/comments',commentRoute)
